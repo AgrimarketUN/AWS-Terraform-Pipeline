9e4b1fe40d73fa625af62d746245c0b004934824
dmirandam <dmirandam@unal.edu.co>
2023-05-27 00:54:43 -0500

    Action

--- main.tf
+++ main.tf
@@ -169,10 +169,10 @@ resource "aws_codepipeline" "cicd-pipeline" {
             version          = "1"
             output_artifacts = ["tf-code"]
             configuration = {
-                FullRepositoryId = "psiquedelos/is2_front"
-                ConnectionArn = var.dockerhub_credentials
+                FullRepositoryId = "AgrimarketUN/AWS-Terraform-Pipeline"
+                BranchName = "main"
+                ConnectionArn = var.codestar_credentials
                 OutputArtifactFormat = "CODE_ZIP"
-                BranchName       = "main"
 
             }
         }
